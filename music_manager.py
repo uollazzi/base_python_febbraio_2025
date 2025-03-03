762e0236e8a087483f47666066ee75138639115c
Matteo Valenzi <dilloawallace@hotmail.com>
2025-03-03 21:18:15 +0100

    init e like

--- music_manager.py
+++ music_manager.py
@@ -3,8 +3,30 @@ import sqlite3
 
 class MusicManager:
     def __init__(self, db_path):
-        # TODO: creare le tabelle se non esistenti
         self.db_path = db_path
+
+        with sqlite3.connect(db_path) as conn:
+            sql_artists = """
+            CREATE TABLE IF NOT EXISTS artists (
+                id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                year_since INTEGER NOT NULL,
+                CONSTRAINT artists_pk PRIMARY KEY (id)
+            );
+            """
+            conn.execute(sql_artists)
+
+            sql_albums = """
+            CREATE TABLE IF NOT EXISTS albums (
+                id INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                release_date INTEGER NOT NULL,
+                artist_id INTEGER NOT NULL,
+                CONSTRAINT albums_pk PRIMARY KEY (id),
+                CONSTRAINT albums_artists_FK FOREIGN KEY (artist_id) REFERENCES artists(id)
+            );
+            """
+            conn.execute(sql_albums)
 
     def add_artist(self, name, year_since):
         with sqlite3.connect(self.db_path) as conn:
@@ -16,14 +38,15 @@ class MusicManager:
                 (name, year_since),
             )
 
-    # TODO: aggiungere la possibilità di cercare
-    def get_artists(self):
+    def get_artists(self, search=""):
         with sqlite3.connect(self.db_path) as conn:
-            return conn.execute(
-                """
-                SELECT * FROM artists
-                """
-            ).fetchall()
+
+            if search != "":
+                sql = "SELECT * FROM artists WHERE name LIKE ?"
+                return conn.execute(sql, (f"%{search}%",)).fetchall()  # %Queen%
+            else:
+                sql = "SELECT * FROM artists"
+                return conn.execute(sql).fetchall()
 
     def add_album(self, title, release_date, artist_id):
         with sqlite3.connect(self.db_path) as conn:
--- spotify.py
+++ spotify.py
@@ -1,13 +1,20 @@
 from music_manager import MusicManager
 from datetime import datetime
 
-m = MusicManager("spotify.db")
 
-m.add_album("Queen", datetime(1973, 7, 13), 2)
+def main():
+    m = MusicManager("spotify.db")
+
+    # m.add_artist("Queen", 1963)
+    # m.add_album("Queen", datetime(1973, 7, 13), 1)
+
+    artists = m.get_artists("Queen")
+
+    print("*** ELENCO ARTISTI ***")
+    for artist in artists:
+        print(f"- {artist[1]}")
 
 
-artists = m.get_artists()
-
-print("*** ELENCO ARTISTI ***")
-for artist in artists:
-    print(f"- {artist[1]}")
+# variabili dunder __main__
+if __name__ == "__main__":
+    main()
